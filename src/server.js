920dd15025ed59018ddca586e674f884ddf2552d
lorexp <vinixp.vp@gmail.com>
2019-11-29 15:40:23 -0300

    adicionados os graficos no pdf, mudança da rota para post

--- src/server.js
+++ src/server.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const pdf_generator = require('./routes/pdf_file');
-const moment = require('moment');
+const express = require("express");
+const pdf_generator = require("./routes/pdf_file");
+const moment = require("moment");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, DataContent");
-    next();
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization, DataContent, chartP, chartR, Accept-Encoding"
+//   );
+//   next();
+// });
+
+app.post("/pdf", (req, res) => {
+  let { capital, lucros, saques, usuario, chartP, chartR } = req.body;
+  let result = lucros.filter(lcr => moment(lcr.data, "DD/MM/YYYY").month() === moment().month());
+
+  pdf_generator(res, capital, result, saques, usuario, chartP, chartR);
 });
 
-
-
-app.get('/pdf', (req, res) => {
-    let {capital, lucros, saques, usuario} = JSON.parse(req.headers.datacontent);
-    let result = lucros.filter(lcr => moment(lcr.data, 'DD/MM/YYYY').month() === moment().month());
-    
-    pdf_generator(res, capital, result, saques, usuario);
-});
-
-
-app.listen(5003, '0.0.0.0', () => console.log('Listening...'));
+app.listen(5003, "0.0.0.0", () => console.log("Listening..."));
