4673a51ee25ed28f343f2c394d6133d75d4d5ddc
lorexp <vinixp.vp@gmail.com>
2020-01-29 15:16:27 -0300

    criado pdf para ethereum

--- src/routes/pdf_file.js
+++ src/routes/pdf_file.js
@@ -4,8 +4,6 @@ const cf = require("currency-formatter");
 const moment = require("moment");
 require("moment/locale/pt-br");
 
-moment.updateLocale("pt-BR", null);
-
 function pdf_generator(res, capital, lucros, saques, usuario, chartP, chartR) {
   const doc = new PDFDocument({
     size: "A4"
@@ -18,7 +16,14 @@ function pdf_generator(res, capital, lucros, saques, usuario, chartP, chartR) {
       .toUpperCase();
 
   //Local para salvar o PDF
-  res.setHeader("Content-disposition", 'attachment; filename="' + usuario.nome + moment().format("MM[_]YYYY") + ".pdf" + '"');
+  res.setHeader(
+    "Content-disposition",
+    'attachment; filename="' +
+      usuario.nome +
+      moment().format("MM[_]YYYY") +
+      ".pdf" +
+      '"'
+  );
   res.setHeader("Content-type", "application/pdf");
   doc.pipe(res);
 
@@ -40,7 +45,9 @@ function pdf_generator(res, capital, lucros, saques, usuario, chartP, chartR) {
   doc.text(fechamento, { align: "center" }); //130, 190
 
   //margem da página
-  doc.rect(20, 230, doc.page.width - 40, doc.page.height - 270).fillAndStroke("#ffffff", "#D1AC00");
+  doc
+    .rect(20, 230, doc.page.width - 40, doc.page.height - 270)
+    .fillAndStroke("#ffffff", "#D1AC00");
 
   //Dados do fechamento do cliente - primeira página
 
@@ -68,7 +75,9 @@ function pdf_generator(res, capital, lucros, saques, usuario, chartP, chartR) {
   doc.moveDown(0.2);
   doc.fill("#000000");
   doc.fontSize(48);
-  doc.text(cf.format(capital.lucro_mensal, { code: "BRL" }), { align: "center" }); //130, 450
+  doc.text(cf.format(capital.lucro_mensal, { code: "BRL" }), {
+    align: "center"
+  }); //130, 450
   doc.fontSize(14);
   doc.moveDown(0.5);
   doc.text("LUCRO MENSAL", { align: "center" });
@@ -78,14 +87,20 @@ function pdf_generator(res, capital, lucros, saques, usuario, chartP, chartR) {
   doc.fontSize(24);
   doc.moveDown(1);
   if (capital.p_lucro_capital >= 0) {
-    doc.fillColor("green").text(capital.p_lucro_capital + "%", { align: "center" });
+    doc
+      .fillColor("green")
+      .text(capital.p_lucro_capital + "%", { align: "center" });
   } else {
-    doc.fillColor("red").text(capital.p_lucro_capital + "%", { align: "center" });
+    doc
+      .fillColor("red")
+      .text(capital.p_lucro_capital + "%", { align: "center" });
   }
   doc.fill("#000000");
   doc.fontSize(48);
   doc.moveDown(0.2);
-  doc.text(cf.format(capital.lucro_capital, { code: "BRL" }), { align: "center" }); //130, 450
+  doc.text(cf.format(capital.lucro_capital, { code: "BRL" }), {
+    align: "center"
+  }); //130, 450
   doc.fontSize(14);
   doc.moveDown(0.5);
   doc.text("CAPITAL INICIAL + LUCRO", { align: "center" });
@@ -119,7 +134,9 @@ function pdf_generator(res, capital, lucros, saques, usuario, chartP, chartR) {
     doc.text(fechamento, { align: "center" }); //130, 190
 
     //margem da página
-    doc.rect(20, 230, doc.page.width - 40, doc.page.height - 270).fillAndStroke("#ffffff", "#D1AC00");
+    doc
+      .rect(20, 230, doc.page.width - 40, doc.page.height - 270)
+      .fillAndStroke("#ffffff", "#D1AC00");
 
     doc.fill("#000000");
     doc.moveDown(2);
@@ -132,9 +149,17 @@ function pdf_generator(res, capital, lucros, saques, usuario, chartP, chartR) {
       doc.fill("#000000");
       doc.text(lucros[x].data, { continued: true, align: "left" });
       if (lucros[x].lucro >= 0) {
-        doc.fillColor("green").text(cf.format(lucros[x].lucro, { code: "BRL" }), { continued: true, align: "right" });
+        doc
+          .fillColor("green")
+          .text(cf.format(lucros[x].lucro, { code: "BRL" }), {
+            continued: true,
+            align: "right"
+          });
       } else {
-        doc.fillColor("red").text(cf.format(lucros[x].lucro, { code: "BRL" }), { continued: true, align: "right" });
+        doc.fillColor("red").text(cf.format(lucros[x].lucro, { code: "BRL" }), {
+          continued: true,
+          align: "right"
+        });
       }
       doc.fill("#000000");
       doc.text(lucros[x].mercado, { lineBreak: true, align: "justify" });
@@ -169,13 +194,17 @@ function pdf_generator(res, capital, lucros, saques, usuario, chartP, chartR) {
   doc.text(fechamento, { align: "center" });
 
   //Caixa onde vão os dados
-  doc.rect(20, 230, doc.page.width - 40, doc.page.height - 270).fillAndStroke("#ffffff", "#D1AC00");
+  doc
+    .rect(20, 230, doc.page.width - 40, doc.page.height - 270)
+    .fillAndStroke("#ffffff", "#D1AC00");
 
   //Grafico Rentabilidade
   doc.image(chartP, 110, 250, { width: 350, height: 250 });
 
   //Faixa abaixo do primeiro grafico
-  doc.rect(50, 510, doc.page.width - 100, 0.5).fillAndStroke("#D1AC00", "#D1AC00");
+  doc
+    .rect(50, 510, doc.page.width - 100, 0.5)
+    .fillAndStroke("#D1AC00", "#D1AC00");
 
   //Grafico ROI
   doc.image(chartR, 110, 550, { width: 350, height: 250 });
@@ -208,7 +237,9 @@ function pdf_generator(res, capital, lucros, saques, usuario, chartP, chartR) {
     doc.text(fechamento, { align: "center" }); //130, 190
 
     //margem da página
-    doc.rect(20, 230, doc.page.width - 40, doc.page.height - 270).fillAndStroke("#ffffff", "#D1AC00");
+    doc
+      .rect(20, 230, doc.page.width - 40, doc.page.height - 270)
+      .fillAndStroke("#ffffff", "#D1AC00");
 
     doc.fill("#000000");
     doc.moveDown(2);
@@ -227,8 +258,14 @@ function pdf_generator(res, capital, lucros, saques, usuario, chartP, chartR) {
       doc.fontSize(14);
       doc.fill("#000000");
       doc.text(saques[x].data, { continued: true, align: "left" });
-      doc.text(cf.format(saques[x].saque, { code: "BRL" }), { continued: true, align: "center" });
-      doc.text(cf.format(saques[x].aplicacao, { code: "BRL" }), { lineBreak: true, align: "right" });
+      doc.text(cf.format(saques[x].saque, { code: "BRL" }), {
+        continued: true,
+        align: "center"
+      });
+      doc.text(cf.format(saques[x].aplicacao, { code: "BRL" }), {
+        lineBreak: true,
+        align: "right"
+      });
 
       if (x !== itens_por_pagina - 1) {
         doc.fill("#adadad");
@@ -296,4 +333,286 @@ function pdf_generator(res, capital, lucros, saques, usuario, chartP, chartR) {
   doc.end();
 }
 
-module.exports = pdf_generator;
+function pdf_ethereum(res, capital, saques, usuario, chartP, chartR) {
+  const doc = new PDFDocument({
+    size: "A4"
+  });
+
+  const fechamento =
+    "FECHAMENTO " +
+    moment()
+      .format("MMMM YYYY")
+      .toUpperCase();
+
+  //Local para salvar o PDF
+  res.setHeader(
+    "Content-disposition",
+    'attachment; filename="' +
+      usuario.nome +
+      moment().format("MM[_]YYYY") +
+      ".pdf" +
+      '"'
+  );
+  res.setHeader("Content-type", "application/pdf");
+  doc.pipe(res);
+
+  //Adicionando a imagem no topo da página
+  doc.image(path.resolve("./src/preta.png"), 145, 0, {
+    fit: [320, 320]
+  });
+
+  //Nome do cliente
+  doc.fontSize(20);
+  doc.moveDown(3.5);
+  doc.text("CLIENTE: " + usuario.nome.toUpperCase(), { align: "center" }); //130, 150
+
+  //Retângulo dourado com o mês de fechamento
+  doc.rect(0, 180, doc.page.width, 30).fill("#D1AC00");
+  doc.fill("#000000");
+  doc.fontSize(25);
+  doc.moveDown(0.3);
+  doc.text(fechamento, { align: "center" }); //130, 190
+
+  //margem da página
+  doc
+    .rect(20, 230, doc.page.width - 40, doc.page.height - 270)
+    .fillAndStroke("#ffffff", "#D1AC00");
+
+  //Dados do fechamento do cliente - primeira página
+
+  //Capital inicial
+  doc.moveDown(1);
+  doc.fill("#000000");
+  doc.text("CAPITAL", { align: "center" });
+  doc.fontSize(48);
+  doc.moveDown(0.5);
+  doc.text(cf.format(capital.inicial, { code: "BRL" }), { align: "center" }); //130, 300
+  doc.fontSize(14);
+  doc.moveDown(0.5);
+  doc.text("CAPITAL INICIAL", { align: "center" }); //240, 360
+  doc.rect(75, 380, doc.page.width - 150, 5).fill("#D1AC00");
+
+  //Lucro mensal
+  doc.fontSize(24);
+  doc.moveDown(1);
+  if (capital.p_mensal >= 0) {
+    doc.fillColor("green").text(capital.p_mensal + "%", { align: "center" });
+  } else {
+    doc.fillColor("red").text(capital.p_mensal + "%", { align: "center" });
+  }
+
+  doc.moveDown(0.2);
+  doc.fill("#000000");
+  doc.fontSize(48);
+  doc.text(cf.format(capital.lucro_mensal, { code: "BRL" }), {
+    align: "center"
+  }); //130, 450
+  doc.fontSize(14);
+  doc.moveDown(0.5);
+  doc.text("LUCRO MENSAL", { align: "center" });
+  doc.rect(75, 530, doc.page.width - 150, 5).fill("#D1AC00");
+
+  //Capital + Lucro
+  doc.fontSize(24);
+  doc.moveDown(1);
+  if (capital.p_lucro_capital >= 0) {
+    doc
+      .fillColor("green")
+      .text(capital.p_lucro_capital + "%", { align: "center" });
+  } else {
+    doc
+      .fillColor("red")
+      .text(capital.p_lucro_capital + "%", { align: "center" });
+  }
+  doc.fill("#000000");
+  doc.fontSize(48);
+  doc.moveDown(0.2);
+  doc.image(path.resolve("./src/ethereum.png"), 90, 575, {
+    fit: [60, 60]
+  });
+  doc.text(cf.format(capital.lucro_capital, { code: "BRL" }), {
+    align: "center"
+  }); //130, 450
+  doc.fontSize(14);
+  doc.moveDown(0.5);
+  doc.text("MOEDA MINERADA ETHEREUM", { align: "center" });
+
+  //incluindo os gráficos no pdf - terceira página
+
+  doc.addPage();
+  //Adicionando a imagem no topo da página
+  doc.image(path.resolve("./src/preta.png"), 145, 0, {
+    fit: [320, 320]
+  });
+
+  //Retângulo dourado com o mês de fechamento
+  doc.rect(0, 180, doc.page.width, 30).fill("#D1AC00");
+  doc.fill("#000000");
+  doc.fontSize(25);
+  doc.moveDown(4);
+  doc.text(fechamento, { align: "center" });
+
+  //Caixa onde vão os dados
+  doc
+    .rect(20, 230, doc.page.width - 40, doc.page.height - 270)
+    .fillAndStroke("#ffffff", "#D1AC00");
+
+  //Grafico Rentabilidade
+  // doc.image(chartP, 110, 250, { width: 350, height: 250 });
+
+  //Faixa abaixo do primeiro grafico
+  doc
+    .rect(50, 510, doc.page.width - 100, 0.5)
+    .fillAndStroke("#D1AC00", "#D1AC00");
+
+  //Grafico ROI
+  // doc.image(chartR, 110, 550, { width: 350, height: 250 });
+
+  // Criação da quarta página
+  qtd_pages = saques.length / 10;
+  diff = qtd_pages % 1;
+  qtd_pages = qtd_pages + 1 - diff;
+  for (let i = 0; i < qtd_pages; i++) {
+    if (saques.length > 10) {
+      itens_por_pagina = 10;
+    } else {
+      if (saques.length === 0) {
+        break;
+      } else {
+        itens_por_pagina = saques.length;
+      }
+    }
+    doc.addPage();
+    //Adicionando a imagem no centro e topo da página
+    doc.image(path.resolve("./src/preta.png"), 145, 0, {
+      fit: [320, 320]
+    });
+
+    //Retângulo dourado com o mês de fechamento
+    doc.rect(0, 180, doc.page.width, 30).fill("#D1AC00");
+    doc.fill("#000000");
+    doc.fontSize(25);
+    doc.moveDown(4);
+    doc.text(fechamento, { align: "center" }); //130, 190
+
+    //margem da página
+    doc
+      .rect(20, 230, doc.page.width - 40, doc.page.height - 270)
+      .fillAndStroke("#ffffff", "#D1AC00");
+
+    doc.fill("#000000");
+    doc.moveDown(2);
+    doc.text("SAQUES/APLICAÇÕES", { align: "center" });
+    //Itens do array
+    doc.fontSize(14);
+    doc.moveDown(1.5);
+
+    doc.text("DATA", { align: "left", continued: true });
+    doc.text("SAQUES", 270, 325, { width: 20, continued: true });
+    doc.text("APLICAÇÃO", 360, 325, { lineBreak: true });
+
+    doc.moveDown(1.5);
+
+    for (let x = 0; x < itens_por_pagina; x++) {
+      doc.fontSize(14);
+      doc.fill("#000000");
+      doc.text(saques[x].data, { continued: true, align: "left" });
+      doc.text(cf.format(saques[x].saque, { code: "BRL" }), {
+        continued: true,
+        align: "center"
+      });
+      doc.text(cf.format(saques[x].aplicacao, { code: "BRL" }), {
+        lineBreak: true,
+        align: "right"
+      });
+
+      if (x !== itens_por_pagina - 1) {
+        doc.fill("#adadad");
+        doc.text("__________________________________________________________", {
+          align: "left",
+          width: doc.page.width,
+          lineBreak: true
+        });
+      }
+
+      doc.moveDown(0.7);
+    }
+    saques = saques.slice(10, saques.length);
+  }
+
+  //Ultima página
+  doc.addPage();
+
+  doc.image(path.resolve("./src/preta.png"), 145, 0, {
+    fit: [320, 320]
+  });
+
+  //Retângulo dourado com o mês de fechamento
+  doc.rect(0, 180, doc.page.width, 30).fill("#D1AC00");
+  doc.fill("#000000");
+  doc.fontSize(25);
+  doc.moveDown(4);
+  doc.text(fechamento, { align: "center" }); //130, 190
+
+  //margem da página
+  doc
+    .rect(20, 230, doc.page.width - 40, doc.page.height - 270)
+    .fillAndStroke("#ffffff", "#D1AC00");
+
+  //Lucro mensal
+  doc.fontSize(18);
+  doc.fill("#000000");
+  doc.moveDown(2);
+  doc.text("TOTAL EM MOEDAS ETHEREUM", { align: "center" });
+  doc.moveDown(2.5);
+  doc.image(path.resolve("./src/ethereum.png"), 170, 320, {
+    fit: [60, 60]
+  });
+  doc.fill("#000000");
+  doc.fontSize(48);
+  doc.text(capital.moedas, {
+    align: "center"
+  }); //130, 450
+  doc.rect(75, 415, doc.page.width - 150, 5).fill("#D1AC00");
+
+  //Capital + Lucro
+  doc.fontSize(24);
+  doc.moveDown(2);
+  if (capital.p_lucro_mensal_total >= 0) {
+    doc
+      .fillColor("green")
+      .text(capital.p_lucro_mensal_total + "%", { align: "center" });
+  } else {
+    doc
+      .fillColor("red")
+      .text(capital.p_lucro_mensal_total + "%", { align: "center" });
+  }
+  doc.fill("#000000");
+  doc.fontSize(48);
+  doc.moveDown(0.2);
+  doc.text(cf.format(capital.lucro_mensal_total, { code: "BRL" }), {
+    align: "center"
+  }); //130, 450
+  doc.fontSize(14);
+  doc.moveDown(0.5);
+  doc.text("LUCRO MENSAL TOTAL", { align: "center" });
+  doc.rect(75, 585, doc.page.width - 150, 5).fill("#D1AC00");
+
+  doc.moveDown(4);
+  doc.fill("#000000");
+  doc.fontSize(48);
+  doc.text(cf.format(capital.moedas * capital.cotacao, { code: "BRL" }), {
+    align: "center"
+  }); //130, 450
+  doc.fontSize(14);
+  doc.moveDown(0.5);
+  doc.text("VALOR TOTAL PARA VENDA HOJE", { align: "center" });
+
+  // Finalize PDF file
+  doc.end();
+}
+
+module.exports = {
+  pdf_generator,
+  pdf_ethereum
+};
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const pdf_generator = require("./routes/pdf_file");
-const moment = require("moment");
+const { pdf_generator, pdf_ethereum } = require("./routes/pdf_file");
 const cors = require("cors");
 
 const app = express();
@@ -8,20 +7,40 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization, DataContent, chartP, chartR, Accept-Encoding"
-//   );
-//   next();
-// });
-
 app.post("/pdf", (req, res) => {
   let { capital, lucros, saques, usuario, chartP, chartR } = req.body;
   //let result = lucros.filter(lcr => moment(lcr.data, "DD/MM/YYYY").month() === moment().month());
 
   pdf_generator(res, capital, lucros, saques, usuario, chartP, chartR);
+});
+
+app.get("/ethereum", (req, res) => {
+  //TROCAR PARA POST LOGO APÓS TERMINAR O PDF
+  // let { capital, lucros, saques, usuario, chartP, chartR } = req.body;
+  const usuario = {
+    nome: "vinicius"
+  };
+
+  const capital = {
+    inicial: 10000,
+    p_mensal: 10,
+    lucro_mensal: 10,
+    p_lucro_capital: 10,
+    lucro_capital: 100000,
+    lucro_mensal_total: 100000,
+    moedas: 10,
+    cotacao: 700,
+    p_lucro_mensal_total: 10
+  };
+
+  const saques = [
+    {
+      data: "20/01/2020",
+      saque: 100,
+      aplicacao: "Moedas"
+    }
+  ];
+  pdf_ethereum(res, capital, saques, usuario, null, null); //trocar null por chartP e chartR
 });
 
 app.listen(5003, "0.0.0.0", () => console.log("Listening..."));
